6ad198df2ca26290fe7c4c9de1703785a2e1ad13
Rao Rudra Pratap <raorudra916@gmail.com>
2026-01-26 17:09:24 +0530

    Solved DNS issue

--- backend/src/lib/env.js
+++ backend/src/lib/env.js
@@ -1,9 +1,20 @@
 import dotenv from 'dotenv';
+import dns from 'node:dns/promises';
+
+// Fix DNS resolution for MongoDB Atlas
+dns.setServers(['1.1.1.1', '8.8.8.8']);
 
 dotenv.config();
 
 export const ENV={
     PORT:process.env.PORT,
     DB_URL:process.env.DB_URL,
-    NODE_ENV:process.env.NODE_ENV
+    NODE_ENV:process.env.NODE_ENV,
+    INGEST_EVENT_KEY:process.env.INGEST_EVENT_KEY,
+    INNGEST_SIGNING_KEY:process.env.INNGEST_SIGNING_KEY,
+    STREAM_API_KEY:process.env.STREAM_API_KEY,
+    STREAM_API_SECRET:process.env.STREAM_API_SECRET,
+    CLERK_PUBLISHABLE_KEY:process.env.CLERK_PUBLISHABLE_KEY,
+    CLERK_SECRET_KEY:process.env.CLERK_SECRET_KEY,
+    CLIENT_URL:process.env.CLIENT_URL
 };
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,24 @@
-import express from 'express';
-import path from 'path';
+import express from 'express'
+import path from 'path'
+import cors from 'cors'
+import {serve} from "inngest/express"
+
 import {ENV} from './lib/env.js'
+import {connectDB} from './lib/db.js'
+import {inngest,functions} from './lib//inngest.js'
 
+ 
 const app=express();
-
+ 
 const __dirname=path.resolve()
+
+app.use(express.json())
+app.use(cors({origin:ENV.CLIENT_URL,credentials:true}))
+
+app.use("api/inngest",serve({
+    client:inngest,
+    functions
+}))
 
 app.get('/home',(req,res)=>{
     res.status(200).json({msg:"succcess from nigga home"});
@@ -12,15 +26,26 @@ app.get('/home',(req,res)=>{
 
 app.get('/about',(req,res)=>{
     res.status(200).json({msg:"succcess from nigga about "});
-});
-
+}); 
+ 
 if(ENV.NODE_ENV==="production"){
     app.use(express.static(path.join(__dirname,"../frontend/dist")))
     app.get("/{*any}",(req,res)=>{
         res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
     });
 }
+    
 
-app.listen(ENV.PORT,()=>{
-    console.log("Server is running on: ",ENV.PORT);
-});
+
+const startServer=async ()=>{
+    try{
+        await connectDB();
+        app.listen(ENV.PORT,()=>{
+            console.log("Server is running on: ",ENV.PORT);
+        }); 
+    }catch(err){ 
+        console.error("Error starting the server",err);
+    }
+}  
+   
+startServer(); 
