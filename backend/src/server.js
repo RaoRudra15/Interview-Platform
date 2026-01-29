f5b7a5e469b570c9f126deabf2f41c5872f1c8c5
Rao Rudra Pratap <raorudra916@gmail.com>
2026-01-29 19:54:44 +0530

    session routes & controllers added

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,31 +2,32 @@ import express from 'express'
 import path from 'path'
 import cors from 'cors'
 import {serve} from "inngest/express"
+import { clerkMiddleware } from '@clerk/express'
 
 import {ENV} from './lib/env.js'
 import {connectDB} from './lib/db.js'
 import {inngest,functions} from './lib//inngest.js'
+import { protectRoute } from './middleware/protectRoute.js'
+import chatRoutes from "./routes/chatRoutes.js"
+import sessionRoutes from "./routes/sessionRoutes.js"
 
- 
+
 const app=express();
  
 const __dirname=path.resolve()
 
 app.use(express.json())
 app.use(cors({origin:ENV.CLIENT_URL,credentials:true}))
+app.use(clerkMiddleware())
 
-app.use("/api/inngest",serve({
-    client:inngest,
-    functions
-}))
+app.use("/api/inngest",serve({client: inngest, functions}))
+app.use("/api/chat",chatRoutes)
+app.use("/api/sessions",sessionRoutes)
+
 
 app.get('/home',(req,res)=>{
     res.status(200).json({msg:"succcess from nigga home"});
 });
-
-app.get('/about',(req,res)=>{
-    res.status(200).json({msg:"succcess from nigga about "});
-}); 
  
 if(ENV.NODE_ENV==="production"){
     app.use(express.static(path.join(__dirname,"../frontend/dist")))
@@ -34,9 +35,7 @@ if(ENV.NODE_ENV==="production"){
         res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
     });
 }
-    
 
- 
 const startServer=async ()=>{
     try{
         await connectDB();
